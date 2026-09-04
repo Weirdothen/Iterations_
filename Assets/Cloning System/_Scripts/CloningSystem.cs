b24d36ff3a0da353d8ec32bd60cee93182f7f814
mohamed20047 <88450499+mohamed20047@users.noreply.github.com>
2026-09-04 23:23:39 +0300

    fix bug after destroy also scale the player down

--- Assets/Cloning System/_Scripts/CloningSystem.cs
+++ Assets/Cloning System/_Scripts/CloningSystem.cs
@@ -1,5 +1,6 @@
 using Clone;
 using UnityEngine;
+using Iterations.Events;
 
 public class CloningSystem : MonoBehaviour
 {
@@ -9,6 +10,10 @@ public class CloningSystem : MonoBehaviour
     [SerializeField] private int cloneSpawningTime = 5;
     [SerializeField, Range(1, 10)] private int captureEveryNFrames = 2;
     [SerializeField] private int maxRecordTime = 500;
+
+    [Header("events Channels")]
+    [SerializeField] private VoidEventChannelSO onLoseTriggered;
+
     private ReplaySystem _system;
 
 
@@ -16,6 +21,17 @@ public class CloningSystem : MonoBehaviour
     {
         _system = new ReplaySystem(this);
     }
+
+    private void OnEnable()
+    { 
+        if (onLoseTriggered != null) onLoseTriggered.OnEventRaised += HandleOnLoseTriggered;
+    }
+
+    private void HandleOnLoseTriggered()
+    {
+        _system.FinishRun();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
